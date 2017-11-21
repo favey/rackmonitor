1dce280fe76a8e7ea5d16ba85c68be721e6f7a08
favey <favey.roff@gmail.com>
2017-11-21 20:33:41 +0800

    initial import

---
+++ src/main/java/com/greywanchuang/rackmonitor/RackmonitorApplication.java
@@ -0,0 +1,12 @@
+package com.greywanchuang.rackmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RackmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RackmonitorApplication.class, args);
+	}
+}
